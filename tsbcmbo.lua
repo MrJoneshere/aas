b0d18a96e27215c72acd137c7af805d172be31af
MrJoneshere <148353445+MrJoneshere@users.noreply.github.com>
2024-07-10 23:09:10 +0300

    Create tsbcmbo.lua

---
+++ tsbcmbo.lua
@@ -0,0 +1,20 @@
+loadstring(
+    game:HttpGet(
+        "https://raw.githubusercontent.com/MrJoneshere/useful-scripts/main/fps%20and%20ping%20counter%20mid%20top%20screen%20android%20and%20pc"
+    )
+)()
+wait(1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/FilteringEnabled/NamelessAdmin/main/Source"))()
+wait(1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MrJoneshere/aas/main/sssss.lua"))()
+wait(1)
+local a = loadstring(game:HttpGet("https://raw.githubusercontent.com/idontknowwhattonamemyself/a/Lua/Games"))()
+for i, v in pairs(a) do
+    if v == game.PlaceId or v == game.GameId then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/idontknowwhattonamemyself/a/Lua/" .. i))()
+    end
+end
+wait(1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+wait(1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/richie0866/orca/master/public/snapshot.lua"))()
