823853fc1ffb9ef15437a53c8adb6946dd85c997
MrJoneshere <148353445+MrJoneshere@users.noreply.github.com>
2025-05-16 23:20:23 +0300

    Update vexon.lua

--- vexon.lua
+++ vexon.lua
@@ -1,14 +1,10 @@
-local vexonHubScript = "https://raw.githubusercontent.com/DiosDi/VexonHub/main/VexonHub"
-local keyUrl = "https://raw.githubusercontent.com/DiosDi/VexonHub/main/Key"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DiosDi/VexonHub/main/VexonHub", true))()
 
-if not pcall(function() readfile("__temp_vexon.lua") end) then
-    loadstring(game:HttpGet(vexonHubScript, true))()
-    
-    if syn then
-        local key = syn.request({Url = keyUrl, Method = "GET"}).Body
-        setclipboard(key)
-        syn.toast_notification("VexonHub", "✅ Key copied: "..string.sub(key, 1, 5).."...", 5)
-    else
-        warn("HTTP engellendi! Key'i manuel al: "..keyUrl)
-    end
-end
+local key = game:HttpGet("https://raw.githubusercontent.com/DiosDi/VexonHub/main/Key", true)
+setclipboard(key)
+
+game:GetService("StarterGui"):SetCore("SendNotification", {
+    Title = "VexonHub",
+    Text = "✅ Key copied!",
+    Duration = 5
+})
