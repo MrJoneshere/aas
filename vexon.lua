6da59e06e1776b921950ac880b94afbc40097bd8
MrJoneshere <148353445+MrJoneshere@users.noreply.github.com>
2025-05-16 23:17:06 +0300

    Update vexon.lua

--- vexon.lua
+++ vexon.lua
@@ -1,6 +1,10 @@
-local HttpService = game:GetService("HttpService")
-HttpService.HttpEnabled = true
-local url = "https://raw.githubusercontent.com/DiosDi/VexonHub/refs/heads/main/Key"
-local key = HttpService:GetAsync(url)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/DiosDi/VexonHub/refs/heads/main/VexonHub"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DiosDi/VexonHub/main/VexonHub"))()
+local key = game:HttpGet("https://raw.githubusercontent.com/DiosDi/VexonHub/main/Key")
 setclipboard(key)
+if game:GetService("Players").LocalPlayer:FindFirstChild("PlayerGui") then
+    game:GetService("Players").LocalPlayer.PlayerGui:SetTopbarTransparency(1)
+    game:GetService("Players").LocalPlayer.PlayerGui:WaitForChild("Notification"):Fire(
+        "✅ Key copied to clipboard!",
+    )
+end
+print("✅ Key copied to clipboard! | " .. key)
