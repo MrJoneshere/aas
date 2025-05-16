017f02fc7b82ca4c4ccba837e269d031ad12cc5d
MrJoneshere <148353445+MrJoneshere@users.noreply.github.com>
2025-05-16 23:19:40 +0300

    Update vexon.lua

--- vexon.lua
+++ vexon.lua
@@ -1,10 +1,14 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/DiosDi/VexonHub/main/VexonHub"))()
-local key = game:HttpGet("https://raw.githubusercontent.com/DiosDi/VexonHub/main/Key")
-setclipboard(key)
-if game:GetService("Players").LocalPlayer:FindFirstChild("PlayerGui") then
-    game:GetService("Players").LocalPlayer.PlayerGui:SetTopbarTransparency(1)
-    game:GetService("Players").LocalPlayer.PlayerGui:WaitForChild("Notification"):Fire(
-        "✅ Key copied to clipboard!",
-    )
+local vexonHubScript = "https://raw.githubusercontent.com/DiosDi/VexonHub/main/VexonHub"
+local keyUrl = "https://raw.githubusercontent.com/DiosDi/VexonHub/main/Key"
+
+if not pcall(function() readfile("__temp_vexon.lua") end) then
+    loadstring(game:HttpGet(vexonHubScript, true))()
+    
+    if syn then
+        local key = syn.request({Url = keyUrl, Method = "GET"}).Body
+        setclipboard(key)
+        syn.toast_notification("VexonHub", "✅ Key copied: "..string.sub(key, 1, 5).."...", 5)
+    else
+        warn("HTTP engellendi! Key'i manuel al: "..keyUrl)
+    end
 end
-print("✅ Key copied to clipboard! | " .. key)
